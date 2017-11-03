750f9ad2ce22385c2dcef939f2999ba01a2ed4ed
Chs97 <623528324@qq.com>
2017-11-03 20:17:47 +0800

    :tada: init project

---
+++ src/pages/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { Button, NavBar, Icon } from 'antd-mobile'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar
+          mode="dark"
+          leftContent="Back"
+          rightContent={[<Icon key="0" type="search" style={{ marginRight: '16px' }} />, <Icon key="1" type="ellipsis" />]}
+        >
+          NavBar
+        </NavBar>
+        <div className="App-header">
+          <Button type="primary">按钮</Button>
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
